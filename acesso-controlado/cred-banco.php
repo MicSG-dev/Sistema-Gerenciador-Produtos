6cddee1aac76b0375ac5857900ce89facde0c8c8
MichelSiG <michelsgalvao@gmail.com>
2022-09-16 10:45:57 -0300

    Adicionado o arquivo com as credenciais do banco
    Adicionado o arquivo com as credenciais do banco de dados e o arquivo de bloqueio de acesso externo direto do arquivo com as credenciais.

---
+++ acesso-controlado/cred-banco.php
@@ -0,0 +1,12 @@
+<?php
+    if(defined("the_cred-banco")){
+        $nome_banco = "*********";
+        $user_banco = "*********";
+        $senha_banco = "*********";
+        $host = "*********";
+        $table_schema = "*********";
+    }else{
+        exit('Acesso bloqueado para acesso direto.');
+    }
+    
+?>
